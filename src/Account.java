1ca7a08c9830902ab796989e3788ecaedcffddd8
adhiambokonyango <marykonyango74@gmail.com>
2020-07-05 18:23:33 +0300

    account

---
+++ src/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    private String name; // instance var
+    // method to set name of object
+    public  void setName(String name)
+    {
+        this.name = name; // store name
+    }
+    public String getName()
+    {
+        return name; // return value of name to caller
+    }
+}// end of class account
---
+++ src/AccountTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args)
+    {
+        //create scanner object to obtain input
+        Scanner input = new Scanner((System.in));
+        // create account object and assign it to my account
+        Account myAccount = new Account();
+
+        // display initial value of name null
+        System.out.printf("initial name is : %s%n%n", myAccount.getName());
+        System.out.println("please enter the name:");
+        String theName = input.nextLine();// read line of text
+        myAccount.setName(theName);// put name in my account
+        // output name stored in my account
+        System.out.printf("Name:%n%s%n", myAccount.getName());
+    }
+}// end of class account test
